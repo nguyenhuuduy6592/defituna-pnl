c6896eb5c46d2c3257d91a570ce6c7ce8b15c34a
DuyNH <duynh@talgent.com>
2025-04-13 00:16:55 +0700

    fix: update service worker registration path from '/service-worker.js' to '/sw.js' for consistency and improved deployment

--- src/__tests__/pages/_app.test.js
+++ src/__tests__/pages/_app.test.js
@@ -81,7 +81,7 @@ describe('App Component (_app.js)', () => {
        map.load();
     });
 
-    expect(navigator.serviceWorker.register).toHaveBeenCalledWith('/service-worker.js');
+    expect(navigator.serviceWorker.register).toHaveBeenCalledWith('/sw.js');
     // We expect the console log upon successful registration (async)
     // Need to await the promise resolution
     return Promise.resolve().then(() => {
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }) {
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
         navigator.serviceWorker
-          .register('/service-worker.js')
+          .register('/sw.js')
           .then(registration => {
             console.log('Service Worker registered:', registration)
           })
