d41899f5d5dbcf68cc12ef5663d543557123e7b5
DuyNH <nguyenhuuduy6592@gmail.com>
2025-12-05 13:01:18 +0700

    chore(config): remove indent rule from ESLint configuration

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -34,7 +34,6 @@ module.exports = [
     rules: {
       semi: ['error', 'always'],
       quotes: ['error', 'single'],
-      indent: ['error', 2],
       'no-unused-vars': 'warn',
       'react/jsx-uses-react': 'off',
       'react/react-in-jsx-scope': 'off',
